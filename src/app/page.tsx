cf7ffacf5d508d2ba67200aab5462b92102ea370
Helena-Bali <elenabaltajs@gmail.com>
2025-06-10 19:50:40 +0300

    reviews template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 "use client"
 
 import { useState, useRef, useEffect } from "react";
+import { Review } from "@/components/Review/Review";
 
+interface ReviewType {
+    id: number;
+    text: string;
+    sanitizedText?: string;
+}
 
 export default function Home() {
   
+    const [reviews, setReviews] = useState<ReviewType[]>([]);
     const [page, setPage] = useState<number>(1);
     const [total, setTotal] = useState<number>(0);
     const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -31,7 +38,7 @@ export default function Home() {
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-start gap-32">
-
+            <Review reviews={reviews} />
             <div className='w-full flex flex-col justify-center items-center'>
               
                 <div ref={sentinelRef} className="h-20" />
---
+++ src/components/Review/Review.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import DOMPurify from 'dompurify';
+import {useEffect, useState} from "react";
+
+interface ReviewType {
+    text: string;
+    sanitizedText?: string;
+}
+
+interface ReviewProps {
+    reviews: ReviewType[];
+}
+
+export const Review: React.FC<ReviewProps> = ({ reviews }) => {
+    const [localReviews, setLocalReviews] = useState<ReviewType[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        if (reviews) {
+            const sanitizedReviews = reviews.map(review => ({
+                ...review,
+                sanitizedText: DOMPurify.sanitize(review.text)
+            }));
+            setLocalReviews(sanitizedReviews);
+        }
+    }, [reviews]);
+
+    useEffect(() => {
+        if (localReviews.length > 0) {
+            setIsLoading(false)
+        }
+    }, [localReviews])
+
+    return (
+        <div className='flex flex-col sm:flex-row gap-5 w-full justify-center'>
+            {isLoading ? (
+                <>
+                    <div className='flex flex-col justify-start items-start bg-[#D9D9D9] rounded-2xl w-full min-h-[611px] max-w-[468px] px-3 py-5 md:pb-8 gap-3 animate-pulse'>
+                        <div className='h-6 bg-gray-300 rounded w-3/4 mb-2'></div>
+                        <div className='h-6 bg-gray-300 rounded w-2/3 mb-2'></div>
+                        <div className='h-6 bg-gray-300 rounded w-1/2'></div>
+                    </div>
+                    <div className='flex flex-col justify-start items-start bg-[#D9D9D9] rounded-2xl w-full min-h-[611px] max-w-[468px] px-3 py-5 md:pb-8 gap-3 animate-pulse'>
+                        <div className='h-6 bg-gray-300 rounded w-3/4 mb-2'></div>
+                        <div className='h-6 bg-gray-300 rounded w-2/3 mb-2'></div>
+                        <div className='h-6 bg-gray-300 rounded w-1/2'></div>
+                    </div>
+                </>
+            ) : (
+                localReviews.map((review, index) => (
+                    <div key={index} className='flex flex-col justify-start mx-auto sm:mx-0 items-start bg-[#D9D9D9] rounded-2xl w-full min-h-[611px] max-w-[468px] px-3 py-5 md:pb-8 gap-3'>
+                        <div dangerouslySetInnerHTML={{__html: review.sanitizedText || ''}} className='text-center sm:text-left text-black text-xl'/>
+                    </div>
+                ))
+            )}
+
+
+        </div>
+    );
+}
