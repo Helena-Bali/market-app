6ccf9afc820ed6187aeec74d47af62b923e145f7
Helena-Bali <elenabaltajs@gmail.com>
2025-06-11 11:02:39 +0300

    fetch products from API

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,8 +3,10 @@
 import Image from "next/image";
 import {useState, useEffect} from "react";
 import {Button} from "@/components/Button/Button";
-import {Product} from "@/types";
+import {useAppDispatch} from "@/store/hooks";
+import {addToCart, removeFromCart} from "@/store/slices/cartSlice";
 import {FormCounter} from "@/components/FormCounter/FormCounter";
+import {Product} from "@/types";
 
 interface CardProps {
     product: Product;
@@ -13,7 +15,7 @@ interface CardProps {
 export const Card: React.FC<CardProps> = ({product}) => {
     const {description, id, image_url, price, title } = product
     const [count, setCount] = useState(0);
-
+    const dispatch = useAppDispatch();
 
     const increment = () => {
         setCount(prevCount => prevCount + 1);
@@ -23,7 +25,14 @@ export const Card: React.FC<CardProps> = ({product}) => {
         setCount(prevCount => prevCount - 1);
     };
 
-    
+    useEffect(() => {
+        if (count > 0) {
+            dispatch(addToCart({product, count}));
+        } else {
+            dispatch(removeFromCart(product.id));
+        }
+    }, [count, dispatch, product]);
+
     return (
         <div className='flex flex-col justify-start items-center bg-[#D9D9D9] rounded-2xl w-full max-w-sm sm:max-w-[301px] px-2.5 py-2 gap-2'>
             <Image
